ce1c9ab5ff4a6553d241a57696c05344d8a48f68
vineethsagar <30525698+vineethsagar@users.noreply.github.com>
2020-11-16 10:21:28 +0530

    change 1

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,1 +1,13 @@
 // write code for Welcome component here
+import React from "react";
+import "../styles/App.css";
+
+export function Welcome(name) {
+  console.log(name);
+  return (
+    <>
+      <h1>Hey ! {name}</h1>
+      <h2>Welcome to Newton School.</h2>
+    </>
+  );
+}
